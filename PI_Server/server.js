4fa019ea2a13dceca8ab6cc99b0f9168a9f35f12
kainklob <klobk23@gmail.com>
2022-10-19 17:46:03 -0400

    base production-ready application. Deployed to fly.io | Requires pi-client to be built

--- PI_Server/server.js
+++ PI_Server/server.js
@@ -3,15 +3,25 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
+// Static route for pi-client index.html
 app.use(express.static(path.join(__dirname, './pi-client/build')));
+
+// API ROUTES
 
 app.get("/test", (req, res) => {
     res.json({ message: "Hello from the PI_Server" });
 });
 
+// Home route to deliver pi-client/build/index.html
+app.get('/', (req, res) => {
+    res.status(200).send();
+})
+
 // Catch-all Route to deliver pi-client React App
-app.get("*", (req, res) => {
-    res.send();
+app.get('/:pageCalled', (req, res) => {
+    res.redirect('/');
 });
+
+// Error Handling (TBW)
 
 app.listen(port, () => console.log(`PI_Server app listening on port ${port}`));
