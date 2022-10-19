b403c2121aa2aad7a38aa701dc63f2767d740a19
kainklob <klobk23@gmail.com>
2022-10-19 17:24:12 -0400

    localhost:3000/ returns pi-client/build/index.html | Must build client before deploying.

--- PI_Server/server.js
+++ PI_Server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './pi-client/build')));
 
 app.get("/test", (req, res) => {
     res.json({ message: "Hello from the PI_Server" });
-})
+});
+
+// Catch-all Route to deliver pi-client React App
+app.get("*", (req, res) => {
+    res.send();
+});
 
 app.listen(port, () => console.log(`PI_Server app listening on port ${port}`));
